7541da82d2636c3d4766e94443e5f59d7410ddde
stargt <stargt@gmail.com>
2023-03-19 10:17:27 +0900

    GTM inserted

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMMPVSH');`,
+          }}></script>
+      </Head>
       <body class="dark:bg-slate-900">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
