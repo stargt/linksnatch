ec3cc5012401473e071c69c0cafa1851213d8cda
stargt <stargt@gmail.com>
2024-09-22 17:23:13 -0700

    install June

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,10 @@
 import '@/styles/globals.css';
+import { AnalyticsBrowser, identify } from '@june-so/analytics-next';
 import { DefaultSeo, NextSeo } from 'next-seo';
 import { publicRuntimeConfig } from 'next.config';
+import { useRouter } from 'next/router';
 import posthog from 'posthog-js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Userpilot } from 'userpilot';
 
 Userpilot.initialize('NX-b34a07ee');
@@ -12,10 +14,49 @@ posthog.init('phc_bAZ7MNaUGk1hM3jeEujkgsI55N4VNAk8TsNNVzWUH5H', {
   person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
 });
 
+let analytics = AnalyticsBrowser.load({
+  writeKey: 'wk-d4070aed462a49e1be1ad80973a9744c',
+});
+
+export function useJune(writeKey) {
+  const [analytics, setAnalytics] = useState(undefined);
+
+  useEffect(() => {
+    const loadAnalytics = async () => {
+      let response = AnalyticsBrowser.load({
+        writeKey,
+      });
+      setAnalytics(response);
+    };
+    loadAnalytics();
+  }, [writeKey]);
+
+  return analytics;
+}
+
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     import('preline');
   }, []);
+
+  const analytics = useJune('wk-d4070aed462a49e1be1ad80973a9744c');
+  const router = useRouter();
+
+  // analytics.identify('USER_ID',{
+  //   email: `test001+${new Date().getDate()}@measured.im`,
+  // });
+
+  useEffect(() => {
+    const handleRouteChange = (url, { shallow }) => {
+      if (analytics) analytics.page(url);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [analytics]);
 
   return (
     <>
